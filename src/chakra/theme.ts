7728b6f249e9aad923834f7871e16a9795552a67
DeviousLab <deviouslab@gmail.com>
2022-07-26 03:16:23 +0400

    Created basic layout and chakra theme
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

---
+++ src/chakra/theme.ts
@@ -0,0 +1,28 @@
+import { extendTheme } from '@chakra-ui/react';
+import '@fontsource/open-sans/300.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/700.css';
+
+const colors = {
+	brand: {
+		100: '#FF3C00',
+	},
+};
+
+const fonts = {
+	body: 'Open Sans, sans-serif',
+};
+
+const styles = {
+	global: () => ({
+		body: {
+			bg: 'gray.200',
+		},
+	}),
+};
+
+const components = {
+  
+}
+
+export const theme = extendTheme({ colors, fonts, styles });
